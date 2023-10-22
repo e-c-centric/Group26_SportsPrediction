94d35f9f4435fd77eb8395f3f8dbf4cadff2bcee
MathiasPascal <elikem.gale-zoyiku@ashesi.edu.gh>
2023-10-22 23:18:36 +0000

    final-0.175

--- predict.py
+++ predict.py
@@ -12,15 +12,11 @@ ensemble = joblib.load(model_path)
 from flask import Flask, render_template, request
 import numpy as np
 
-# Load your machine learning model
-ensemble = joblib.load('model.pkl')
-
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        # Define the attributes you want to collect
         attributes = [
             'movement_reactions', 'mentality_composure', 'passing',
             'release_clause_eur', 'dribbling', 'wage_eur', 'power_shot_power',
@@ -28,13 +24,11 @@ def index():
             'physic', 'skill_long_passing', 'age'
         ]
 
-        # Collect attribute values from the form
         input_data = {}
         for attr in attributes:
             value = float(request.form.get(attr, 0))
             input_data[attr] = value
 
-        # Use your machine learning model to make predictions
         prediction = round(ensemble.predict([list(input_data.values())]))
 
         return render_template("result.html", prediction=prediction)
